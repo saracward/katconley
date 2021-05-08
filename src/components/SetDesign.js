0d7016cedb8dc2d4a3fe96e0c4db56fc1ecdd43f
Sara Ward <saraward@MacBook-Pro-6.local>
2021-05-08 12:38:29 -0400

    main pages added

---
+++ src/components/SetDesign.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const setdesign = () => {
+    return (
+        <div>
+            <p>hello set design page
+            </p>
+        </div>
+    )
+}
+
+export default setdesign
