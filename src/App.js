a198df30125815201a2d62d7a4d241ba3dcb5a97
Sara Ward <saraward@MacBook-Pro-6.local>
2021-05-25 09:41:38 -0400

    attempt fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Switch, Route } from "react-router-dom"
+import {useState} from 'react'
 import Navbar from "./components/Navbar"
 import Landing from "./components/Landing"
 import About from "./components/About"
@@ -9,7 +10,21 @@ import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function Component(props){
+  const [apiData,setApiData] = useEffect({})
 
+  const getData = async () => {
+    const response = await fetch("https://cdn.contentful.com/spaces/rgcbkr9elt8o/environments/master/content_types/setDesign?access_token=cLMVrw5z3-NKZeTgkmLZzkSVARbXGPo6pwRD-PmL6LU&content_type=setDesign")
+    const data = await response.json()
+    setApiData(data)
+  }
+
+  useEffect(()=> {
+    getData()
+  }, [])
+
+  return <h1> {apiData?.results[0]?.name} </h1>
+}
 
 function App() {
   return (
